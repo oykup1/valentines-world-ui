d343ba7d666428eb04cf328e1d4dc939752d96bb
Oyku <oykupul@gmail.com>
2026-02-14 14:56:16 -0500

    Supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+console.log("URL:", supabaseUrl)
+console.log("KEY exists:", !!supabaseKey)
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
-console.log("URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log("KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
